8eec992e3d3ea080f49f4dbbfa5c9d0fa924c62e
Debjani Paul <debjanideb@hotmail.com>
2020-12-15 22:04:00 -0800

    First commit with components

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import AddEntry from './components/AddEntry';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import History from './components/History'
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <Provider store={createStore(reducer)}>
+        <View style={{ flex: 1 }}>
+          {/* <AddEntry /> */}
+          <History />
+        </View>
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
